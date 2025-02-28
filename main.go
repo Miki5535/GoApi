b9e8d181a1c8e5ee5a661de34550ce9c7bba2c0a
Miki5535 <masterfin787@gmail.com>
2025-02-28 21:17:40 +0700

    fix: update for vercel deployment

--- main.go
+++ main.go
@@ -1,23 +1,48 @@
+// package main
+
+// import (
+// 	"go-sql/db"
+// 	"go-sql/handlers"
+
+// 	"github.com/gin-gonic/gin"
+// )
+
+// func main() {
+// 	db.InitDB()
+
+// 	r := gin.Default()
+
+// 	// กำหนดเส้นทาง API
+// 	r.GET("/country", handlers.GetAllcountrys)
+// 	r.GET("/country/:id", handlers.GetcountryById)
+// 	r.POST("/country", handlers.Addcountry)
+// 	r.PUT("/country/:id", handlers.Updatecountry)
+// 	r.DELETE("/country/:id", handlers.Deletecountry)
+
+// 	r.Run(":8080") // API จะรันที่ http://localhost:8080
+// }
+
 package main
 
 import (
 	"go-sql/db"
 	"go-sql/handlers"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	db.InitDB()
-
-	r := gin.Default()
+	router := gin.Default()
 
 	// กำหนดเส้นทาง API
-	r.GET("/country", handlers.GetAllcountrys)
-	r.GET("/country/:id", handlers.GetcountryById)
-	r.POST("/country", handlers.Addcountry)
-	r.PUT("/country/:id", handlers.Updatecountry)
-	r.DELETE("/country/:id", handlers.Deletecountry)
+	router.GET("/country", handlers.GetAllcountrys)
+	router.GET("/country/:id", handlers.GetcountryById)
+	router.POST("/country", handlers.Addcountry)
+	router.PUT("/country/:id", handlers.Updatecountry)
+	router.DELETE("/country/:id", handlers.Deletecountry)
 
-	r.Run(":8080") // API จะรันที่ http://localhost:8080
+	// ใช้ `ServeHTTP()` เพื่อให้ Vercel ใช้ Gin เป็น Handler
+	router.ServeHTTP(w, r)
 }
